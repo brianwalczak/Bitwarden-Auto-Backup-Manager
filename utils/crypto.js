518155c717bd618ad62f44e980a900f7abdf9c33
Brian <72092838+BrianWalczak@users.noreply.github.com>
2026-02-06 23:07:42 -0600

    Small fixes for crypto.js, prettier.

--- utils/crypto.js
+++ utils/crypto.js
@@ -2,207 +2,217 @@ const crypto = require('crypto');
 
 // Reference to encryption types used (helps identify encryption type from b64)
 const encTypes = {
-    AesCbc256_B64: 0,
-    AesCbc128_HmacSha256_B64: 1,
-    AesCbc256_HmacSha256_B64: 2,
-    Rsa2048_OaepSha256_B64: 3,
-    Rsa2048_OaepSha1_B64: 4,
-    Rsa2048_OaepSha256_HmacSha256_B64: 5,
-    Rsa2048_OaepSha1_HmacSha256_B64: 6,
+  AesCbc256_B64: 0,
+  AesCbc128_HmacSha256_B64: 1,
+  AesCbc256_HmacSha256_B64: 2,
+  Rsa2048_OaepSha256_B64: 3,
+  Rsa2048_OaepSha1_B64: 4,
+  Rsa2048_OaepSha256_HmacSha256_B64: 5,
+  Rsa2048_OaepSha1_HmacSha256_B64: 6,
 }
 
 // Creates an array buffer from a string
 function fromUtf8(str) {
-    const encoder = new TextEncoder();
-    return encoder.encode(str).buffer;
+  const encoder = new TextEncoder();
+  return encoder.encode(str).buffer;
 }
 
 // Converts a buffer to a string
 function toUtf8(buf) {
-    const decoder = new TextDecoder();
-    return decoder.decode(buf);
+  const decoder = new TextDecoder();
+  return decoder.decode(buf);
 }
 
 // Converts a buffer to base 64 format
 function toB64(buf) {
-    let binary = '';
-    const bytes = new Uint8Array(buf);
-    for (let i = 0; i < bytes.byteLength; i++) {
-        binary += String.fromCharCode(bytes[i]);
-    }
-    return Buffer.from(binary, 'binary').toString('base64');
+  let binary = '';
+  const bytes = new Uint8Array(buf);
+
+  for (let i = 0; i < bytes.byteLength; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+
+  return Buffer.from(binary, 'binary').toString('base64');
 }
 
 // Creates a byte data class from a buffer
 class ByteData {
-    constructor(buf) {
-        if (!arguments.length) {
-            this.arr = null;
-            this.b64 = null;
-            return;
-        }
-
-        this.arr = new Uint8Array(buf);
-        this.b64 = toB64(buf);
+  constructor(buf) {
+    if (!arguments.length) {
+      this.arr = null;
+      this.b64 = null;
+      return;
     }
+
+    this.arr = new Uint8Array(buf);
+    this.b64 = toB64(buf);
+  }
 }
 
 // Uses the pbkdf2 algorithm to derive a key from login
 async function pbkdf2(password, salt, iterations, length) {
-    const importAlg = {
-        name: 'PBKDF2',
-    };
+  const importAlg = {
+    name: 'PBKDF2',
+  };
 
-    const deriveAlg = {
-        name: 'PBKDF2',
-        salt: salt,
-        iterations: iterations,
-        hash: { name: 'SHA-256' },
-    };
+  const deriveAlg = {
+    name: 'PBKDF2',
+    salt: salt,
+    iterations: iterations,
+    hash: { name: 'SHA-256' },
+  };
 
-    const aesOptions = {
-        name: 'AES-CBC',
-        length: length,
-    };
+  const aesOptions = {
+    name: 'AES-CBC',
+    length: length,
+  };
 
-    const importedKey = await crypto.subtle.importKey('raw', password, importAlg, false, ['deriveKey']);
-    const derivedKey = await crypto.subtle.deriveKey(deriveAlg, importedKey, aesOptions, true, ['encrypt']);
-    const exportedKey = await crypto.subtle.exportKey('raw', derivedKey);
+  const importedKey = await crypto.subtle.importKey('raw', password, importAlg, false, ['deriveKey']);
+  const derivedKey = await crypto.subtle.deriveKey(deriveAlg, importedKey, aesOptions, true, ['encrypt']);
+  const exportedKey = await crypto.subtle.exportKey('raw', derivedKey);
 
-    return new ByteData(exportedKey);
+  return new ByteData(exportedKey);
 }
 
 // Simple function for HKDF expansion
 async function hkdfExpand(prk, info, size) {
-    const alg = {
-        name: 'HMAC',
-        hash: { name: 'SHA-256' },
-    };
-    const importedKey = await crypto.subtle.importKey('raw', prk, alg, false, ['sign']);
-    const hashLen = 32; // SHA-256 output length
-    const okm = new Uint8Array(size);
-    let previousT = new Uint8Array(0);
-    const n = Math.ceil(size / hashLen);
-    for (let i = 0; i < n; i++) {
-        const t = new Uint8Array(previousT.length + info.length + 1);
-        t.set(previousT);
-        t.set(info, previousT.length);
-        t.set([i + 1], t.length - 1);
-        previousT = new Uint8Array(await crypto.subtle.sign(alg, importedKey, t));
-        okm.set(previousT, i * hashLen);
-    }
+  const alg = {
+    name: 'HMAC',
+    hash: { name: 'SHA-256' },
+  };
 
-    return okm;
+  const importedKey = await crypto.subtle.importKey('raw', prk, alg, false, ['sign']);
+
+  const hashLen = 32; // SHA-256 output length
+  const okm = new Uint8Array(size);
+  let previousT = new Uint8Array(0);
+  const n = Math.ceil(size / hashLen);
+
+  for (let i = 0; i < n; i++) {
+    const t = new Uint8Array(previousT.length + info.length + 1);
+
+    t.set(previousT);
+    t.set(info, previousT.length);
+    t.set([i + 1], t.length - 1);
+
+    previousT = new Uint8Array(await crypto.subtle.sign(alg, importedKey, t));
+    okm.set(previousT, i * hashLen);
+  }
+
+  return okm;
 }
 
 // Derives a master key from any login and iteration count
 async function deriveMasterKey(email, masterPassword, iterations) {
-    // Convert email and password to ArrayBuffer
-    const emailBuffer = fromUtf8(email);
-    const passwordBuffer = fromUtf8(masterPassword);
+  // Convert email and password to ArrayBuffer
+  const emailBuffer = fromUtf8(email);
+  const passwordBuffer = fromUtf8(masterPassword);
 
-    // Use PBKDF2 to derive a key from the buffers with a specific amount ofiterations
-    const masterKey = await pbkdf2(passwordBuffer, emailBuffer, iterations, 256);
+  // Use PBKDF2 to derive a key from the buffers with a specific amount of iterations
+  const masterKey = await pbkdf2(passwordBuffer, emailBuffer, iterations, 256);
 
-    return masterKey;
+  return masterKey;
 }
 
 // Stretches a master key into 64 bytes
 async function stretchKey(masterKey) {
-    const keyBytes = new Uint8Array(masterKey.arr);
-    
-    // Stretching the master key using HKDF
-    const encKey = await hkdfExpand(keyBytes, new Uint8Array(fromUtf8('enc')), 32); // 32 bytes for encryption key
-    const macKey = await hkdfExpand(keyBytes, new Uint8Array(fromUtf8('mac')), 32); // 32 bytes for MAC key
-    
-    // Concatenate the keys
-    const stretchedKey = new Uint8Array(64);
-    stretchedKey.set(encKey);
-    stretchedKey.set(macKey, 32);
+  const keyBytes = new Uint8Array(masterKey.arr);
 
-    return new ByteData(stretchedKey.buffer);
+  // Stretching the master key using HKDF
+  const encKey = await hkdfExpand(keyBytes, new Uint8Array(fromUtf8('enc')), 32); // 32 bytes for encryption key
+  const macKey = await hkdfExpand(keyBytes, new Uint8Array(fromUtf8('mac')), 32); // 32 bytes for MAC key
+
+  // Concatenate the keys
+  const stretchedKey = new Uint8Array(64);
+  stretchedKey.set(encKey);
+  stretchedKey.set(macKey, 32);
+
+  return new ByteData(stretchedKey.buffer);
 }
 
 // Derives your master password hash from your master key and password
 async function deriveMasterPasswordHash(masterKey, masterPassword) {
-    const passwordBuffer = fromUtf8(masterPassword); // Convert password to ArrayBuffer
+  const passwordBuffer = fromUtf8(masterPassword); // Convert password to ArrayBuffer
 
-    // Use PBKDF2 again to derive a new key from the previous PBKDF2 key and password buffer
-    const masterPasswordHash = await pbkdf2(masterKey.arr.buffer, passwordBuffer, 1, 256);
+  // Use PBKDF2 again to derive a new key from the previous PBKDF2 key and password buffer
+  const masterPasswordHash = await pbkdf2(masterKey.arr.buffer, passwordBuffer, 1, 256);
 
-    return masterPasswordHash;
+  return masterPasswordHash;
 }
 
 // Computes an HMAC (Hash-based Message Authentication Code) using SHA-256 algorithm.
 async function computeMac(data, key) {
-    const alg = {
-      name: 'HMAC',
-      hash: { name: 'SHA-256' },
-    }
-    const importedKey = await crypto.subtle.importKey('raw', key, alg, false, ['sign'])
-    return crypto.subtle.sign(alg, importedKey, data)
+  const alg = {
+    name: 'HMAC',
+    hash: { name: 'SHA-256' },
+  };
+
+  const importedKey = await crypto.subtle.importKey('raw', key, alg, false, ['sign']);
+  return crypto.subtle.sign(alg, importedKey, data);
 }
 
 // Checks if two macs equal each other
 async function macsEqual(mac1Data, mac2Data, key) {
-    const alg = {
-      name: 'HMAC',
-      hash: { name: 'SHA-256' },
+  const alg = {
+    name: 'HMAC',
+    hash: { name: 'SHA-256' },
+  };
+
+  const importedMacKey = await crypto.subtle.importKey('raw', key, alg, false, ['sign']);
+  const mac1 = await crypto.subtle.sign(alg, importedMacKey, mac1Data);
+  const mac2 = await crypto.subtle.sign(alg, importedMacKey, mac2Data);
+
+  if (mac1.byteLength !== mac2.byteLength) {
+    return false;
+  }
+
+  const arr1 = new Uint8Array(mac1)
+  const arr2 = new Uint8Array(mac2)
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
     }
+  }
 
-    const importedMacKey = await crypto.subtle.importKey('raw', key, alg, false, ['sign'])
-    const mac1 = await crypto.subtle.sign(alg, importedMacKey, mac1Data)
-    const mac2 = await crypto.subtle.sign(alg, importedMacKey, mac2Data)
-
-    if (mac1.byteLength !== mac2.byteLength) {
-      return false
-    }
-
-    const arr1 = new Uint8Array(mac1)
-    const arr2 = new Uint8Array(mac2)
-
-    for (let i = 0; i < arr2.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false
-      }
-    }
-
-    return true
+  return true;
 }
 
 // Concatenates IV and ciphertext to build data for HMAC
 function buildDataForMac(ivArr, ctArr) {
-    const dataForMac = new Uint8Array(ivArr.length + ctArr.length)
-    dataForMac.set(ivArr, 0)
-    dataForMac.set(ctArr, ivArr.length)
-    return dataForMac
+  const dataForMac = new Uint8Array(ivArr.length + ctArr.length);
+
+  dataForMac.set(ivArr, 0);
+  dataForMac.set(ctArr, ivArr.length);
+
+  return dataForMac;
 }
 
 // Decrypts with AES-CBC using any cipher and encryption + HMAC keys
 async function aesDecrypt(cipher, encKey, macKey) {
-    const keyOptions = {
-      name: 'AES-CBC',
-    }
+  const keyOptions = {
+    name: 'AES-CBC',
+  };
 
-    const decOptions = {
-      name: 'AES-CBC',
-      iv: cipher.iv.arr.buffer,
-    }
+  const decOptions = {
+    name: 'AES-CBC',
+    iv: cipher.iv.arr.buffer,
+  };
 
-    const checkMac = cipher.encType != encTypes.AesCbc256_B64
+  const checkMac = cipher.encType != encTypes.AesCbc256_B64
 
-    if (checkMac) {
-      if (!macKey) throw new Error('Decryption error: MAC key was not provided or is invalid.');
+  if (checkMac) {
+    if (!macKey) throw new Error('Decryption error: MAC key was not provided or is invalid.');
 
-      const dataForMac = buildDataForMac(cipher.iv.arr, cipher.ct.arr)
-      const macBuffer = await computeMac(dataForMac.buffer, macKey.arr.buffer)
-      const macsMatch = await macsEqual(cipher.mac.arr.buffer, macBuffer, macKey.arr.buffer)
+    const dataForMac = buildDataForMac(cipher.iv.arr, cipher.ct.arr);
+    const macBuffer = await computeMac(dataForMac.buffer, macKey.arr.buffer);
+    const macsMatch = await macsEqual(cipher.mac.arr.buffer, macBuffer, macKey.arr.buffer);
 
-      if (!macsMatch) throw new Error('Decryption error: MAC check failed (likely incorrect password).');
-      const importedKey = await crypto.subtle.importKey('raw', encKey.arr.buffer, keyOptions, false, ['decrypt']);
+    if (!macsMatch) throw new Error('Decryption error: MAC check failed (likely incorrect password).');
+    const importedKey = await crypto.subtle.importKey('raw', encKey.arr.buffer, keyOptions, false, ['decrypt']);
 
-      return crypto.subtle.decrypt(decOptions, importedKey, cipher.ct.arr.buffer)
-    }
+    return crypto.subtle.decrypt(decOptions, importedKey, cipher.ct.arr.buffer);
+  }
 }
 
 // Used to create key from the encryption key and "key" value
@@ -227,103 +237,103 @@ async function aesDecryptKey(data, iv, key) {
 
 // Function which utilizes the encryption key and "key" value to create a new byte symmetric key
 async function decryptToBytes(encThing, key) {
-    if (key == null) {
-      throw new Error("No encryption key provided.");
-    }
+  if (key == null) {
+    throw new Error("Decryption error: No encryption key provided.");
+  }
 
-    if (encThing == null) {
-      throw new Error("Nothing provided for decryption.");
-    }
+  if (encThing == null) {
+    throw new Error("Decryption error: Nothing provided for decryption.");
+  }
 
-    key = resolveLegacyKey(key, encThing);
+  key = resolveLegacyKey(key, encThing);
 
-    if (key.macKey != null && encThing.macBytes == null) {
+  if (key.macKey != null && encThing.macBytes == null) {
+    return null;
+  }
+
+  if (key.encType !== encThing.encryptionType) {
+    return null;
+  }
+
+  if (key.macKey != null && encThing.macBytes != null) {
+    const macData = new Uint8Array(encThing.ivBytes.byteLength + encThing.dataBytes.byteLength);
+    macData.set(new Uint8Array(encThing.ivBytes), 0);
+    macData.set(new Uint8Array(encThing.dataBytes), encThing.ivBytes.byteLength);
+
+    const computedMac = await computeMac(macData, key.macKey);
+    if (computedMac === null) {
       return null;
     }
 
-    if (key.encType !== encThing.encryptionType) {
+    const macsMatch = await macsEqual(encThing.macBytes, computedMac, new Uint8Array(crypto.randomBytes(32))); // Check if HMACs equal each other with random byte array
+
+    if (!macsMatch) {
       return null;
     }
+  }
 
-    if (key.macKey != null && encThing.macBytes != null) {
-      const macData = new Uint8Array(encThing.ivBytes.byteLength + encThing.dataBytes.byteLength);
-      macData.set(new Uint8Array(encThing.ivBytes), 0);
-      macData.set(new Uint8Array(encThing.dataBytes), encThing.ivBytes.byteLength);
-      const computedMac = await computeMac(macData, key.macKey);
-      if (computedMac === null) {
-        return null;
-      }
-      
-      const macsMatch = await macsEqual(encThing.macBytes, computedMac, new Uint8Array(crypto.randomBytes(32))); // Check if HMACs equal each other with random byte array
+  const result = await aesDecryptKey(
+    encThing.dataBytes, // the data bytes of the "key" encryption key
+    encThing.ivBytes, // the iv bytes of the "key" encryption key
+    key.encKey, // the original vault encryption key
+  );
 
-      if (!macsMatch) {
-        this.logMacFailed("mac failed.");
-        return null;
-      }
-    }
-
-    const result = await aesDecryptKey(
-      encThing.dataBytes, // the data bytes of the "key" encryption key
-      encThing.ivBytes, // the iv bytes of the "key" encryption key
-      key.encKey, // the original vault encryption key
-    );
-    
-    return result ?? null;
+  return result ?? null;
 }
 
 // Converts a base64 encrypted string into a valid cipher
 class EncCipher {
-    constructor(encryptedString) {
-        const [encTypeAndIV, ct, mac] = encryptedString.split('|');
-        const [encType, iv] = encTypeAndIV.split('.');
+  constructor(encryptedString) {
+    const [encTypeAndIV, ct, mac] = encryptedString.split('|');
+    const [encType, iv] = encTypeAndIV.split('.');
 
-        this.ct = new ByteData(Buffer.from(ct, 'base64'));
-        this.encType = Number(encType);
-        this.iv = new ByteData(Buffer.from(iv, 'base64'));
-        this.mac = new ByteData(Buffer.from(mac, 'base64'));
-        this.string = encryptedString;
-    }
+    this.ct = new ByteData(Buffer.from(ct, 'base64'));
+    this.encType = Number(encType);
+    this.iv = new ByteData(Buffer.from(iv, 'base64'));
+    this.mac = new ByteData(Buffer.from(mac, 'base64'));
+    this.string = encryptedString;
+  }
 }
 
 // Splits a base64 symmetric key into an encryption key and HMAC key
 class SimpleSymmetricCryptoKey {
-    constructor(b64) {
-      if (!arguments.length) {
-        this.key = new ByteData()
-        this.encKey = new ByteData()
-        this.macKey = new ByteData()
-        return
-      }
-
-      this.key = new ByteData(Buffer.from(b64, 'base64'))
-
-      // First half
-      const encKey = this.key.arr.slice(0, this.key.arr.length / 2).buffer
-      this.encKey = new ByteData(encKey)
-
-      // Second half
-      const macKey = this.key.arr.slice(this.key.arr.length / 2).buffer
-      this.macKey = new ByteData(macKey)
+  constructor(b64) {
+    if (!arguments.length) {
+      this.key = new ByteData()
+      this.encKey = new ByteData()
+      this.macKey = new ByteData()
+      return
     }
+
+    this.key = new ByteData(Buffer.from(b64, 'base64'))
+
+    // First half
+    const encKey = this.key.arr.slice(0, this.key.arr.length / 2).buffer
+    this.encKey = new ByteData(encKey)
+
+    // Second half
+    const macKey = this.key.arr.slice(this.key.arr.length / 2).buffer
+    this.macKey = new ByteData(macKey)
+  }
 }
 
 
 module.exports = {
-    encTypes,
-    fromUtf8,
-    toUtf8,
-    toB64,
-    ByteData,
-    pbkdf2,
-    hkdfExpand,
-    deriveMasterKey,
-    stretchKey,
-    deriveMasterPasswordHash,
-    computeMac,
-    macsEqual,
-    buildDataForMac,
-    aesDecrypt,
-    EncCipher,
-    SimpleSymmetricCryptoKey,
-    decryptToBytes
+  encTypes,
+  fromUtf8,
+  toUtf8,
+  toB64,
+  ByteData,
+  pbkdf2,
+  hkdfExpand,
+  deriveMasterKey,
+  stretchKey,
+  deriveMasterPasswordHash,
+  computeMac,
+  macsEqual,
+  buildDataForMac,
+  aesDecrypt,
+  EncCipher,
+  SimpleSymmetricCryptoKey,
+  decryptToBytes
 };
