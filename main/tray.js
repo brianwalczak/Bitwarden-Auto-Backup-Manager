a2dd9511bb924b1d8aae3f5c56f6f4128be8b5d0
Brian <72092838+BrianWalczak@users.noreply.github.com>
2026-03-03 20:50:15 -0600

    Use template image for tray icon (macOS)

--- main/tray.js
+++ main/tray.js
@@ -7,15 +7,33 @@ import { showWindow } from "./window.js";
 let tray = null; // holds the tray instance
 let statusCache = null; // holds cached status to prevent unnecessary tray updates
 
+function getTrayIcon() {
+    let iconPath = app.isPackaged ? path.join(process.resourcesPath, 'icon.png') : path.join(import.meta.dirname, "..", "build", 'icon.png');
+    
+    // use a smaller template image so it looks right
+    if (process.platform === 'darwin') {
+        iconPath = app.isPackaged ? path.join(process.resourcesPath, 'trayTemplate.png') : path.join(import.meta.dirname, "..", "build", 'trayTemplate.png');
+    }
+
+    const icon = nativeImage.createFromPath(iconPath);
+
+    if (process.platform === 'darwin') {
+        icon.setTemplateImage(true);
+        return icon;
+    }
+
+    return icon.resize({ width: 32, height: 32 });
+}
+
 function spawnTray() {
     if (tray) return; // prevent multiple tray instances
 
-    const iconFull = nativeImage.createFromPath(app.isPackaged ? path.join(process.resourcesPath, 'icon.png') : path.join(import.meta.dirname, "..", "build", 'icon.png'));
-    const icon = iconFull.resize({ width: 32, height: 32 });
-    
-    tray = new Tray(icon);
+    tray = new Tray(getTrayIcon());
     tray.setToolTip("Bitwarden Auto-Backup Manager");
-    tray.on("click", showWindow);
+    
+    if (process.platform === "win32") {
+        tray.on("click", showWindow);
+    }
 }
 
 // Creates the system tray app icon
